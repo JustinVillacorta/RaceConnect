12cea21485550098468351c3ccd046a82ba55c52
AkotosiJUSTIN <127915260+AkotosiJUSTIN@users.noreply.github.com>
2025-01-31 21:28:04 +0800

    added implementation for gson and okhttp.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,12 @@ android {
 }
 
 dependencies {
-// retrofit
+    // retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
 
 
 
